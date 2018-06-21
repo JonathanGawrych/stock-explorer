372fc5799a942208739a7027f9cfb1c052b9736b
Jonathan Gawrych <Jonathan.Gawrych@gmail.com>
2018-06-20 17:00:56 -0700

    Change MetaData so that its more succinct

--- src/app/app.module.metadata.ts
+++ src/app/app.module.metadata.ts
@@ -1,44 +1,31 @@
 import { core as Interfaces } from '@angular/compiler';
 
+interface Metadata extends Interfaces.NgModule {
+	// removes duplicates from the module metadata
+	clean():void;
+}
+
 // create metadata with empty arrays so we just need to push
-const metadata: Interfaces.NgModule = {
+const metadata: Metadata = {
 	providers: [],
 	declarations: [],
 	imports: [],
 	exports: [],
 	entryComponents: [],
 	bootstrap: [],
-	schemas: []
+	schemas: [],
+
+	clean() {
+		this.providers = [...new Set(this.providers)];
+		this.declarations = [...new Set(this.declarations)];
+		this.imports = [...new Set(this.imports)];
+		this.exports = [...new Set(this.exports)];
+		this.entryComponents = [...new Set(this.entryComponents)];
+		this.bootstrap = [...new Set(this.bootstrap)];
+		this.schemas = [...new Set(this.schemas)];
+	}
 };
 
 export default metadata;
 
-export function clean(oldMetadata: Interfaces.NgModule) {
-	const newMetadata: Interfaces.NgModule = {};
 
-	if (oldMetadata.providers.length > 0)
-		newMetadata.providers = [...new Set(oldMetadata.providers)];
-
-	if (oldMetadata.declarations.length > 0)
-		newMetadata.declarations = [...new Set(oldMetadata.declarations)];
-
-	if (oldMetadata.imports.length > 0)
-		newMetadata.imports = [...new Set(oldMetadata.imports)];
-
-	if (oldMetadata.exports.length > 0)
-		newMetadata.exports = [...new Set(oldMetadata.exports)];
-
-	if (oldMetadata.entryComponents.length > 0)
-		newMetadata.entryComponents = [...new Set(oldMetadata.entryComponents)];
-
-	if (oldMetadata.bootstrap.length > 0)
-		newMetadata.bootstrap = [...new Set(oldMetadata.bootstrap)];
-
-	if (oldMetadata.schemas.length > 0)
-		newMetadata.schemas = [...new Set(oldMetadata.schemas)];
-
-	if (oldMetadata.id != null)
-		newMetadata.id = oldMetadata.id;
-
-	return newMetadata;
-}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { default as metadata, clean } from 'app.module.metadata';
+import metadata from 'app.module.metadata';
 metadata.imports.push(BrowserModule);
 
 import 'app.component';
 import 'app-routing.module';
 
-@NgModule(clean(metadata))
+metadata.clean();
+@NgModule(metadata)
 export class AppModule { }
 
