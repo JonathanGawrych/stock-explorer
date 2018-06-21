1b478fdbf806324899501ffe304926c0aeccfc03
Jonathan Gawrych <Jonathan.Gawrych@gmail.com>
2018-06-21 02:41:54 -0700

    Style the company information

--- src/app/parts/company-summary/company-summary.component.ts
+++ src/app/parts/company-summary/company-summary.component.ts
@@ -12,6 +12,20 @@ export class CompanySummaryComponent {
 	@Input() set symbol(value: string) {
 		this.company = Stock.Company.get(value);
 	}
+
+	issueTypeToReadable(issueType: Stock.Company.IssueType) {
+		switch (issueType)
+		{
+		case 'ad': return 'American Depository Receipt (ADR’s)'
+		case 're': return 'Real Estate Investment Trust (REIT’s)'
+		case 'ce': return 'Closed end fund (Stock and Bond Fund)'
+		case 'si': return 'Secondary Issue'
+		case 'lp': return 'Limited Partnerships'
+		case 'cs': return 'Common Stock'
+		case 'et': return 'Exchange Traded Fund (ETF)'
+		default: return null;
+		}
+	}
 }
 
 import metadata from 'app.module.metadata';
