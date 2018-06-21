092f8f53180b6b73ce08b340bc2fadca63f47d41
Jonathan Gawrych <Jonathan.Gawrych@gmail.com>
2018-06-20 17:14:59 -0700

    Add heroku to serve the app to the world

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+	res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+// Start the app by listening on the default
+// Heroku port
+const port = process.env.PORT || 4200;
+console.log("starting on port " + port);
+app.listen(port);
